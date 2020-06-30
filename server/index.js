fd7dd2bf0d1bf4669de7a283d9758d402c1a2601
isaacoduh <ufuomaoduh@gmail.com>
2020-06-30 13:33:30 +0100

    Main File Initialization Server[index.js]

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+require('./models/user.model');
+require('./helpers/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(cookieSession({maxAge: 86400, keys: [keys.cookieKey]}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/auth.routes')(app);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
