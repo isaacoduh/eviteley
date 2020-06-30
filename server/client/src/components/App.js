98f53bbab42ff50508d284e383f28e87089c0737
isaacoduh <ufuomaoduh@gmail.com>
2020-06-30 16:21:50 +0100

    Client: Setup Base Browser Links, Setup Base Reducers including authReducer, Setup React-Router and Added links, Added header component in the display hierachy

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h3>Dashboard</h3>;
+const EviteNew = () => <h2>EviteNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
     return (
-        <div>Hi There!</div>
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <Route exact path="/"  component={Landing}/>
+                <Route exact path="/evites" component={Dashboard} />
+                <Route path="/evites/new" component={EviteNew}/>
+            </div>
+        </BrowserRouter>
     );
 };
 
